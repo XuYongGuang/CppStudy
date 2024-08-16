cbda5051990e7f58735f44f62f61f516a32e6de0
xuyongguang <18859663916@163.com>
2024-08-16 22:24:02 +0800

    pointer

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,20 @@
 // // #include "Debug/Debug.h"
 // #include "Reference/Reference.h"
 // #include "ClassAndStruct/ClassAndStruct.h"
-#include "Static/Static.h"
-
-#include "Pointers/Pointers.h"
+// #include "Static/Static.h"
+// #include "Pointers/Pointers.h"
 
 int main(int, char**){
     // CompilerAndLinkerTest();
     // VariablesTest();
     // FunctionsTest();
     // DebugTest();
+    // PointersTest();
     // ReferenceTest();
     // ClassAndStructTest();
-    StaticTest(); printf("main s_var = %d \n", s_var);
+    // StaticTest(); printf("main s_var = %d \n", s_var);
 
 
-    PointersTest();
     printf("Test Done! \n ");
     // std::cin.get();
 }
